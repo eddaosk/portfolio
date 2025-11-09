a6f9a5eaa275d23ec640a41710fb6036b189fe66
jbb10 <johann.bjorn.bjornsson@gmail.com>
2025-11-09 14:37:43 +0100

    added fallback year for improved accessibility, deleted code that was not in use anymore

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
 // Wait for the DOM to be loaded
 document.addEventListener("DOMContentLoaded", function () {
-    // Add smooth scrolling to all links
-    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-        anchor.addEventListener("click", function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute("href")).scrollIntoView({
-                behavior: "smooth",
-            });
-        });
-    });
-
-    // Add fade-in animation to elements with 'fade-in' class
-    const fadeElements = document.querySelectorAll(".fade-in");
-
-    const fadeInObserver = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-            }
-        });
-    });
-
-    fadeElements.forEach((element) => {
-        fadeInObserver.observe(element);
-    });
-
     // Set current year in footer(s)
     const currentYear = new Date().getFullYear();
     document.querySelectorAll(".js-current-year").forEach((el) => {
