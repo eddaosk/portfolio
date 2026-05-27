52b0143f8ca186709fd0c613c709052429a38d2d
eddaosk <e.o.oskarsdottir@gmail.com>
2026-05-27 23:36:16 +0200

    converting from source files to dist files

--- vasuqi/src/animations/constants.js
+++
@@ -1,3 +0,0 @@
-export const WATER_DURATION = { fast: 0.8, default: 1.0, slow: 1.2 }
-export const WATER_EASE = 'power1.inOut'
-export const WATER_STAGGER = 0.12
--- vasuqi/src/config.js
+++
@@ -1,2 +0,0 @@
-// All founder-configurable values live here. Only contact.js imports this file.
-export const FORMSPREE_ENDPOINT = 'https://formspree.io/f/YOUR_FORM_ID'
